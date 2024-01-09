e3442875f30777ddb1c3794dde1f98295b90e9a6
peter michael <enitanpeters28@gmail.com>
2024-01-09 15:27:35 +0100

    single item page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,29 @@
 <template>
     <MainLayout>
         <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2"> 
-            Inx pAg
+            <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+                <div v-if="products" v-for="product in products" :key="product">
+                    <ProductComponent :product="product"/>
+                </div>
+            </div>
         </div>
     </MainLayout>
 </template>
 
 <script setup lang="ts">
 import MainLayout from '~/layouts/MainLayout.vue'
+
+// let products = ref(null)
+export let products: any[] = [];
+for (let i = 0; i < 10; i++) {
+   let product = {
+       id: i + 1, // assuming ids start from 1
+       title: `Product ${i + 1}`,
+       description: `Description for Product ${i + 1}`,
+       url: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6CmNjCyjmLxpDzaYTrneBOLu1_MgKeMjgcg&usqp=CAU`,
+       price: 100 // random price between 0 and 100
+   };
+   products.push(product);
+}
+// products = productsArr
 </script>
-
-<style scoped>
-
-</style>
