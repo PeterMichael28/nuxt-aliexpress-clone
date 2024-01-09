c2afb363502f1ad3e8b321268e57ef9349482b07
peter michael <enitanpeters28@gmail.com>
2024-01-09 16:56:27 +0100

    footr

--- components/Footer.vue
+++ components/Footer.vue
@@ -29,7 +29,7 @@
 
     <div id="FooterSub" class="w-full bg-[#333333] text-sm text-gray-400">
         <div class="max-w-[1200px] mx-auto p-4">
-            Intellectual Property Protection - Privacy Policy - Sitemap - Terms of Use - Information for EU consumers - Transaction Services Agreement for non-EU/UK Consumers - Terms and Conditions for EU/EEA/UK Consumers (Transactions) - User Information Legal Enquiry Guide ©️2010-2022 AliExpress.com. All rights reserved.  增值电信业务经营许可证 浙B2-20120091-8  浙公网安备 33010802002248号
+            Intellectual Property Protection - Privacy Policy - Sitemap - Terms of Use - Information for EU consumers - Transaction Services Agreement for non-EU/UK Consumers - Terms and Conditions for EU/EEA/UK Consumers (Transactions) - User Information Legal Enquiry Guide ©️2010-2024 AliExpress.com. All rights reserved.  增值电信业务经营许可证 浙B2-20120091-8  浙公网安备 33010802002248号
         </div>
     </div>
 </template>
